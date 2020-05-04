b188e1efdb641751d15e35d674bf0cc0f62ff076
novakvova <novakvova@gmail.com>
2020-05-04 10:19:09 +0300

    Begin create project game

---
+++ ServerMD/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServerMD.Models;
+
+namespace ServerMD.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class GameController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            var collider = new
+            {
+                pos= new MyPosVextor3 { X= -11.8f, Y=-8.2f, Z=0.0f },
+                velocity = new MyPosVextor3 { X = 14.7f, Y=17.5f, Z = 0.0f }
+            };
+
+            return Ok(collider);
+        }
+    }
+}
---
+++ ServerMD/Models/MyPosVextor3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerMD.Models
+{
+    public class MyPosVextor3
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+    }
+}
